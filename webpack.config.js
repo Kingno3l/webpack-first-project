c21304d1040e2d146735ed9648fade80318e6d1a
kingnoel <66368728+kingnoel@users.noreply.github.com>
2023-02-02 11:40:07 +0100

    well done

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  optimization: {
+    runtimeChunk: "single",
   },
 };
